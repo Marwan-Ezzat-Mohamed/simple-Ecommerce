52112138411017f199d082f1ceea7ff63f105032
AmiraHisham20 <amirahisham.258@gmail.com>
2022-01-02 13:45:01 +0200

    Checkout

--- react-front-end/src/components/Checkout/index.js
+++ react-front-end/src/components/Checkout/index.js
@@ -1,5 +1,7 @@
 import { Container} from "react-bootstrap";
 import {Form} from 'react-bootstrap';
+import image from "../../assets/visas_logos.png";
+import imag from "../../assets/see-the-source-image-cash-on-delivery-now-available-11563353301vi2pno7jfy.png";
 
 const Checkout = () => {
   return <div className="checkout">
@@ -45,8 +47,6 @@ const Checkout = () => {
     <Form.Control type="email"/>
     </Form.Group>
    </Form>
-   <Form.Check type='checkbox' className="form-group"/>
-   <small>Text me updates about my Best Buy order.</small>
     </div>
 </div>
 
@@ -57,8 +57,6 @@ const Checkout = () => {
   <div className="card-block bg-white py-2 px-2 ">
     <Container>
       <h5 className="card-title"> Order Summary</h5>
-     {/* <p className="card-text">Item total Store backup</p>
-      <h5 className="card-title">Order Summary</h5>*/}
       <hr />
       <table className="card-body">
         <tr>
@@ -75,6 +73,86 @@ const Checkout = () => {
       <p>Total</p>
     </Container>
   </div>
+</div>
+<div
+  className="card mt-4 my-4 right"
+  style={{ width: "25rem", height: "17rem" }}
+>
+  <div className="card-block bg-white py-2 px-2 ">
+    <Container>
+      <h5 className="card-title"> Payment Method</h5>
+      <hr />
+       <p>
+         How do you want to pay for your order?
+       </p>
+        
+       <div class="form-check">
+       <img src={image} width={80} height={30}/>
+         <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+         <label class="form-check-label" for="flexRadioDefault1">
+            By credit card
+         </label>
+         
+         <Form >
+      <Form.Group className="form-group">
+         <b>Card Number</b>
+          <Form.Control type="text"/>
+        </Form.Group>
+        <Form.Group className="form-group">
+          <b>Card Holder Name</b>
+          <Form.Control type="text"/>
+        </Form.Group>
+        <Form.Group className="form-group">
+          <b>CVV</b>
+          <Form.Control type="text"/>
+        </Form.Group>
+        </Form>
+         <h6>
+            Expired Date
+          </h6>
+        <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+         
+            <option selected>Year</option>
+            <option value="1">2023</option>
+            <option value="2">2024</option>
+            <option value="3">2025</option>
+            <option value="4">2026</option>
+            <option value="5">2027</option>
+            <option value="6">2028</option>
+            <option value="7">2029</option>
+            <option value="8">2030</option>
+        </select>
+        <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+         
+         <option selected>Month</option>
+         <option value="1">1.January</option>
+         <option value="2">2.Febraury</option>
+         <option value="3">3.March</option>
+         <option value="4">4.April</option>
+         <option value="5">5.May</option>
+         <option value="6">6.June</option>
+         <option value="7">7.July</option>
+         <option value="8">8.August</option>
+         <option value="9">9.Septemper</option>
+         <option value="10">10.October</option>
+         <option value="11">11.November</option>
+         <option value="12">12.December</option>
+     </select>
+       </div><br></br>
+       <div class="form-check">
+       <img src={imag} width={50} height={30}/>
+         <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
+         <label class="form-check-label" for="flexRadioDefault2">
+            On Delivery
+         </label>
+        </div><br></br>
+        
+        <div class="col-auto" >
+          
+         <button type="submit" class="btn btn-primary mb-3">Confirm Order</button>
+       </div>
+   </Container>
+ </div>
 </div>
 
   </div>;
